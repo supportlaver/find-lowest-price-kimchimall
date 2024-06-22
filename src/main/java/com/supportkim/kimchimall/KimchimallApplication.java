94175bae1e0bf3b250a9065af0073fd7dd5cbc86
supportlaver <ak4686@naver.com>
2024-06-22 23:12:33 +0900

    settings

---
+++ src/main/java/com/supportkim/kimchimall/KimchimallApplication.java
@@ -0,0 +1,13 @@
+package com.supportkim.kimchimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimchimallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimchimallApplication.class, args);
+	}
+
+}
